01d6437c1cbefeccaa2d7eb83b29bbe848881a18
min <cdr9898@naver.com>
2025-06-16 15:40:30 +0900

    전체수정

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,15 @@
-/* 
-		var swiper = new Swiper(".project_Swiper", {
-				spaceBetween: 30,
-				loop: true,
-				pagination: {
-						el: ".swiper-pagination",
-						clickable: true,
-				},
-		});
 
- */
-
-/* froject 스와이퍼 */
-let swiper = new Swiper(".frobox1", {
-	slidesPerView: 3,
+/* design 스와이퍼 */
+let swiper = new Swiper(".design_1", {
+	slidesPerView: 4,
 	centeredSlides: false,
 	loop: true,
-	spaceBetween: 15,
+    speed: 6000,
+    autoplay: {
+        delay: 1,
+    },
+    freemode: true,
+	spaceBetween: 20,
 	pagination: {
 		el: ".swiper-pagination",
 		type: "fraction",
@@ -25,15 +19,77 @@ let swiper = new Swiper(".frobox1", {
 		prevEl: ".swiper-button-prev",
 	},
 	breakpoints: {
-        920: {
-            slidesPerView: 3,
+        769: {
+            slidesPerView: 4,
         },
         600: {
-            slidesPerView: 2,
+            slidesPerView: 3,
         },
         0: {
-            slidesPerView: 1,
+            slidesPerView: 2,
         },
+
     },
 
+});
+
+
+/* 모바일 버튼 */
+const mobtn = document.getElementById('mobtn');
+const moslide = document.getElementById('moslide');
+const closeButton = document.getElementById('closebtn');
+const mobg = document.getElementById('mobg');
+const molist = moslide.querySelectorAll('ul li');
+
+
+mobtn.addEventListener('click', function() {
+    moslide.classList.add('active');
+    mobg.classList.add('active');
+});
+
+closeButton.addEventListener('click', function() {
+    moslide.classList.remove('active');
+    mobg.classList.remove('active');
+});
+
+mobg.addEventListener('click', function() {
+	moslide.classList.remove('active');
+	mobg.classList.remove('active');
+});
+
+molist.forEach(item => {
+    item.addEventListener('click', function() {
+        moslide.classList.remove('active');
+        mobg.classList.remove('active');
+    });
+});
+
+/* project 모달창 */
+
+// 모달 열기 버튼들
+document.querySelectorAll('.pormodal_openbtn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        const targetSelector = btn.getAttribute('data-target');
+        const modal = document.querySelector(targetSelector);
+        modal.classList.add('show');
+        document.body.style.overflow = 'hidden'; // 스크롤 차단
+    });
+});
+
+// 닫기 버튼들
+document.querySelectorAll('.pormodal_colsebtn ').forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.closest('.promodal').classList.remove('show');
+        document.body.style.overflow = '';
+    });
+});
+
+// 모달 배경 클릭 시 닫기
+document.querySelectorAll('.promodal').forEach(modal => {
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+        }
+    });
 });
